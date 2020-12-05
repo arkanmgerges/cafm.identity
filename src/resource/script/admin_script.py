03ef2224b5e12ac843502946e4e57c9ca1ab82d5
Arkan M. Gerges <arkan.m.gerges@gmail.com>
2020-12-05 21:00:42 +0200

    Make kafka partitions count as variable

--- src/resource/script/admin_script.py
+++ src/resource/script/admin_script.py
@@ -351,7 +351,7 @@ def createUser(db, id, name, password):
 def init_kafka_topics_and_schemas():
     # Create topics
     topics = ['cafm.identity.cmd', 'cafm.identity.evt']
-    newTopics = [NewTopic(topic, num_partitions=1, replication_factor=1) for topic in topics]
+    newTopics = [NewTopic(topic, num_partitions=os.getenv('KAFKA_PARTITIONS_COUNT_PER_TOPIC', 1), replication_factor=1) for topic in topics]
     # admin = AdminClient({'bootstrap.servers': os.getenv('MESSAGE_BROKER_SERVERS', '')})
     admin = AdminClient({'bootstrap.servers': 'kafka:9092'})
     fs = admin.create_topics(newTopics)
