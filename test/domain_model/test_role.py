456df9d711fa21897a91232323794fb632b569a9
Mohammad Sobhani <moso@develoop.run>
2021-01-22 13:36:03 +0330

    add tests

--- test/domain_model/test_role.py
+++ test/domain_model/test_role.py
@@ -18,26 +18,27 @@ def test_create_role_with_semantic_constructor():
     # Arrange
     id = str(uuid4())
     # Act
-    role = Role.createFrom(id=id, name='Prj1')
+    role = Role.createFrom(id=id, name='Prj1', title="Title")
     # Assert
     assert isinstance(role, Role)
     assert isinstance(role, Resource)
     assert role.type() == 'role'
     assert role.id() == id
     assert role.name() == 'Prj1'
+    assert role.title() == 'Title'
 
 
 def test_that_two_objects_with_same_attributes_are_equal():
     # Act
-    object1 = Role.createFrom('1234', 'test')
-    object2 = Role.createFrom('1234', 'test')
+    object1 = Role.createFrom('1234', 'test', 'title')
+    object2 = Role.createFrom('1234', 'test', 'title')
     # Assert
     assert object1 == object2
 
 
 def test_that_two_objects_with_different_attributes_are_not_equal():
     # Act
-    object1 = Role.createFrom('1234', 'test')
-    object2 = Role.createFrom('1234', 'test2')
+    object1 = Role.createFrom('1234', 'test', 'title')
+    object2 = Role.createFrom('1234', 'test2', 'title2')
     # Assert
     assert object1 != object2
