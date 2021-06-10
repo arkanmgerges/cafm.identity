e2bc6fba986731a47c1625ab6a05b44b93046a4d
Arkan M. Gerges <arkan.m.gerges@gmail.com>
2021-06-10 15:11:51 +0300

    Fix roleTree error

--- src/resource/script/helper/tree/parser.py
+++ src/resource/script/helper/tree/parser.py
@@ -100,7 +100,7 @@ class TreeParser:
 
     # end permission tree region
 
-    def parseRoleTree(self, role_tree):
+    def parseRoleTree(self, roleTree):
         roleId = self.cafmClient.ensureRoleExistence(
             name=roleTree["name"],
             title=roleTree["title"],
